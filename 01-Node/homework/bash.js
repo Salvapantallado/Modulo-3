e951a910d642963cc123d3fc18467f1ef40db173
Liwi <lighu.miranda@gmail.com>
2026-09-24 21:44:32 -0300

    01 xd

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,15 +1,19 @@
-const commands = require('./commands');
-const date = 'date'
+const commands = require('./commands/index.js');
+
 // Output un prompt
 process.stdout.write('prompt > ');
 // El evento stdin 'data' se dispara cuando el user escribe una línea
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remueve la nueva línea
   if(cmd === 'date') {
-   commands[date]() 
+   commands.date()
+  // commands['date']() 
   }
   if(cmd === 'pwd') {
-    process.stdout.write(process.execPath);
+    commands.pwd()
+  }
+  if(cmd.startsWith('echo')) {
+    commands.echo(cmd)
   }
   process.stdout.write('\nprompt > ');
 });
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,1 +1,5 @@
-module.exports = {date: function showDate() {process.stdout.write(Date())}};
+module.exports = {
+    date: function showDate() {process.stdout.write(Date())},
+    pwd: function showDirectory() {process.stdout.write(process.execPath)},
+    echo: function showEcho(data) {process.stdout.write(data.slice(4))}
+};
